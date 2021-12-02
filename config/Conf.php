13c11ffbf327ad0c82a3e75471340bc41153ef80
pantecouteaut <theo.pantecouteau@icloud.com>
2021-12-02 16:10:27 +0100

    initial commit

---
+++ config/Conf.php
@@ -0,0 +1,53 @@
+<?php
+class Conf {
+
+    static private $databases = array(
+        // Le nom d'hote est webinfo a l'IUT
+        // ou localhost sur votre machine
+        'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
+        // A l'IUT, vous avez une BDD nommee comme votre login
+        // Sur votre machine, vous devrez creer une BDD
+        'database' => 'pantecouteaut',
+        // A l'IUT, c'est votre login
+        // Sur votre machine, vous avez surement un compte 'root'
+        'login' => 'pantecouteaut',
+        // A l'IUT, c'est votre mdp (INE par defaut)
+        // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+        'password' => '100977084BD'
+    );
+
+    // la variable debug est un boolean
+    static private $debug = True;
+
+    static public function getDebug() {
+        return self::$debug;
+    }
+
+    static public function getLogin() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['login'];
+    }
+    static public function gethostname() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['hostname'];
+    }
+    static public function getDatabase() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['database'];
+    }
+    static public function getPassword() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['password'];
+    }
+
+    public static function myGet($nonvar) {
+        if (isset($_GET[$nonvar])) {
+            return $_GET[$nonvar];
+        }
+        else if(isset($_POST[$nonvar])) {
+            return $_POST[$nonvar];
+        }
+       
+        else return null;
+    }
+}
---
+++ model/Model.php
@@ -0,0 +1,171 @@
+<?php
+require_once File::build_path(array("config", "Conf.php"));
+
+class Model
+{
+
+    private static $pdo = NULL;
+
+    public static function init()
+    {
+        try {
+            $hostname = Conf::gethostname();
+            $datebase_name = Conf::getDatabase();
+            $login = Conf::getLogin();
+            $password = Conf::getPassword();
+
+            self::$pdo = new PDO("mysql:host=$hostname;dbname=$datebase_name", $login, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            } else {
+                echo "Une erreur est survenue";
+            }
+            die();
+        }
+    }
+
+    public static function getPDO()
+    {
+        if (is_null(self::$pdo)) {
+            self::init();
+        }
+        return self::$pdo;
+    }
+
+    public static function selectAll()
+    {
+        $table_name = static::$object;
+        $class_name = 'Model' . ucfirst($table_name);
+        try {
+            $rep = Model::getPDO()->query("SELECT * FROM $table_name");
+            $rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            return $tab = $rep->fetchAll();
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+
+    public static function select($primary_value)
+    {
+        $table_name = static::$object;
+        $class_name = 'Model' . ucfirst($table_name);
+        $primary_key = static::$primary;
+        try {
+            $sql = "SELECT * from $table_name WHERE $primary_key=:nom_tag";
+            // Préparation de la requête
+            $req_prep = Model::getPDO()->prepare($sql);
+
+            $values = array(
+                "nom_tag" => $primary_value,
+            );
+            // On donne les valeurs et on exécute la requête
+            $req_prep->execute($values);
+
+            // On récupère les résultats comme précédemment
+            $req_prep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+            $tab = $req_prep->fetchAll();
+            // Attention, si il n'y a pas de résultats, on renvoie false
+            if (empty($tab))
+                return false;
+            return $tab[0];
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+
+    public static function delete($primary_value)
+    {
+        $table_name = static::$object;
+        $primary_key = static::$primary;
+        try {
+            $sql = "DELETE FROM $table_name WHERE $primary_key=:tag1";
+            $req_prep = Model::getPDO()->prepare($sql);
+            $values = array(
+                "tag1" => $primary_value
+            );
+            $req_prep->execute($values);
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+
+    public static function update($data)
+    {
+        $table_name = static::$object;
+        $primary_key = static::$primary;
+        $set = "";
+        $tab = array();
+        foreach ($data as $cle => $valeur) {
+            if ($cle == $primary_key) {
+                $primary_value = $valeur;
+                $tab[":primary_key"] = $valeur;
+            } else {
+                $set = $set . $cle . "=" . ":" . $cle . ",";
+                $tab[":$cle"] = $valeur;
+            }
+        }
+        $set = rtrim($set, ",");
+        $sq = $set . " " . "WHERE " . $primary_key .  "=:" . 'primary_key';
+        try {
+            $sql = "UPDATE $table_name SET $sq";
+            $req_prep = Model::getPDO()->prepare($sql);
+            $req_prep->execute($tab);
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+
+    public static function save()
+    {
+        $table_name = static::$object;
+        $attributs = static::$data;
+        $set = "";
+        $tag = "";
+        foreach ($attributs as $key => $valeur) {
+            if ($key != 'numProduit' || $key != 'idCommande') {
+                $set = $set . $key . ",";
+                $tag = $tag . ":$key" . ",";
+            }
+        }
+        $set = rtrim($set, ",");
+        $tag = rtrim($tag, ",");
+        try {
+            $sql = "INSERT INTO $table_name (" . $set . ") VALUES
+                    (" . $tag . ") ";
+
+            $req_prep = Model::getPDO()->prepare($sql);
+
+            // On donne les valeurs et on exécute la requête
+            $req_prep->execute($attributs);
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+        }
+    }
+}
