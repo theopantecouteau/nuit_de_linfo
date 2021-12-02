b5579f9bd4443a74301ee7aae69edbacf76e48d1
JulienFerrandiz <jferrandiz34@gmail.com>
2021-12-02 16:28:14 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-require_once __DIR__ . DIRECTORY_SEPARATOR;
-require_once file::build_path(array("controller", "routeur.php"));
+session_start();
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (10*60))) {
+    session_unset();
+    session_destroy();
+} else {
+    $_SESSION['LAST_ACTIVITY'] = time();
+}
+$ROOT_FOLDER = __DIR__;
+$DS = DIRECTORY_SEPARATOR;
+require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
+require_once File::build_path(array("controller","routeur.php"));
 ?>
