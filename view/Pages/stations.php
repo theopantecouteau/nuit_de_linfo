bc42da04a0e8eed77773ab4be48ee1464493f072
Noe Desprez <noe.desprez@etu.umontpellier.fr>
2021-12-03 03:39:34 +0100

    stations

---
+++ view/Pages/stations.php
@@ -0,0 +1,69 @@
+<article class="single-page-article clr">
+
+
+    <div class="entry clr" itemprop="text">
+
+
+
+        <p>/</p>
+
+
+
+        <h1 class="has-text-align-center"><strong>Les 5 stations de sauvetage en mer du dunkerquois</strong></h1>
+
+
+
+        <p></p>
+
+
+
+        <p>La première station du littoral dunkerquois est créée en 1834 par la Société Humaine de Dunkerque à&nbsp;&nbsp;<strong>Dunkerque.</strong></p>
+
+
+
+        <p></p>
+
+
+
+        <p>En 1866 la station de&nbsp;<strong>Gravelines</strong>&nbsp;est créée sous l’égide de la Société Centrale de Sauvetage des Naufragés (SCSN).</p>
+
+
+
+        <p></p>
+
+
+
+        <p>En 1872 la Société Humaine de Dunkerque rejoint la SCSN.</p>
+
+
+
+        <p></p>
+
+
+
+        <p>En 1879 La&nbsp; SCNS crée une&nbsp; station à&nbsp;&nbsp;<strong>Fort-Mardyck&nbsp;</strong>qui fermera en 1919.</p>
+
+
+
+        <p></p>
+
+
+
+        <p>En 1891 La&nbsp;SCNS crée une&nbsp; station à&nbsp;<strong>&nbsp;Malo-les-Bains</strong>&nbsp;qui fermera en 1904.</p>
+
+
+
+        <p></p>
+
+
+
+        <p>En 1959 les Hospitaliers Bretons créent une station à&nbsp;<strong>Bray-Dunes</strong> – Station fermée de nos jours.</p>
+
+
+
+        <p></p>
+
+
+    </div>
+
+</article>
