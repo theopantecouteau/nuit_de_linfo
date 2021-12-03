6f11a0c95beb513a8879ddef3b049620984ad199
thomas-frias <77983279+thomas-frias@users.noreply.github.com>
2021-12-03 04:28:03 +0100

    on bon chinois sait faire un travail de qualité

---
+++ view/Pages/phares-et-balises.php
@@ -0,0 +1,243 @@
+<article class="single-page-article clr">
+
+
+    <div class="entry clr" itemprop="text">
+
+
+
+        <h1 class="has-text-align-center"><strong>Phares, feux flottants et bateaux-phares</strong></h1>
+
+
+
+        <figure class="wp-block-image size-large"><img loading="lazy" width="1024" height="503" src="https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque-1024x503.jpg" alt="" class="wp-image-4542" srcset="https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque-1024x503.jpg 1024w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque-300x147.jpg 300w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque-768x377.jpg 768w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque-1536x754.jpg 1536w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bancs-et-feux-de-Dunkerque.jpg 1878w" sizes="(max-width: 1024px) 100vw, 1024px" /></figure>
+
+
+
+        <div class="wp-block-columns">
+            <div class="wp-block-column">
+                <p></p>
+
+
+
+                <p></p>
+
+
+
+                <p><sup><em>Les bateaux-feux dunkerquois sont rapidement devenus des éléments indispensables à la sécurité des navigateurs. Ils seront d&rsquo;ailleurs sollicités dans quelques sauvetages du 19e siècle.</em></sup><br><sup><em>En permanence au mouillage au large de Dunkerque il était logique que la <strong>Société Centrale de Sauvetage des Naufragés</strong> y installe à demeure des équipements de sauvetage.</em></sup></p>
+            </div>
+
+
+
+            <div class="wp-block-column">
+                <figure class="wp-block-image size-full"><img loading="lazy" width="960" height="690" src="https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bateaux-feux-a-quai.jpg" alt="" class="wp-image-4543" srcset="https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bateaux-feux-a-quai.jpg 960w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bateaux-feux-a-quai-300x216.jpg 300w, https://sauveteurdudunkerquois.fr/wp-content/uploads/2021/10/Bateaux-feux-a-quai-768x552.jpg 768w" sizes="(max-width: 960px) 100vw, 960px" /><figcaption>Bateaux feux a quai</figcaption></figure>
+            </div>
+        </div>
+
+
+
+        <h2>1842 Phare du Risban</h2>
+
+
+
+        <p>Deux nouveaux feux sont installés sur le littoral du département du Nord, un à Dunkerque, l&rsquo;autre à Gravelines. <br>Les indications ci-après font connaitre la position, le caractère et la portée de ces deux feux.</p>
+
+
+
+        <p> <br><strong>FEU DE DUNKERQUE. </strong><br>Feu tournant à éclipses se succédant de minute en minute. A partir du mois de mars prochain, l&rsquo;ancien phare à feu fixe de la tour du Leughenaer sera supprimé et remplacé par un phare à éclipses de minute en minute, qui sera allumé sur la tour récemment construite entre la grande écluse de chasse et le Fort-Risban, à 1400 mètres au N.O. de la tour du Leughenaer par 51°3 de latitude et 0° 1&prime; 41&Prime; de longitude E. <br>La tour du nouveau phare de Dunkerque a 55 mètres de hauteur au-dessus du sol, et le foyer de l&rsquo;appareil de l&rsquo;éclairage domine de 59 mètres le niveau des plus hautes marées. Dans un beau temps les éclats du nouveau phare pourront être aperçus jusqu&rsquo;à la distance de 24 milles marins (40 à 45 km) par un observateur placé à 10 ou 12 mètres au-dessus de la surface de la mer. <br>Il est essentiel d&rsquo;observer que les éclipses ne paraîtront totales en temps ordinaire qu&rsquo;au-delà d&rsquo;une distance de 12 à 15 milles marins. Au-delà de cette distance, on apercevra un petit feu fixe dans les intervalles des grands éclats. </p>
+
+
+
+        <p><br><strong>FEU DE GRAVELINES. </strong> <br>Feu fixe. Ce feu sera allumé sur la tour récemment construite à l&rsquo;est des entrées des jetées de Gravelines  par 51° 0&prime; 17&Prime; de latitude et 0° 13&prime; 38&Prime; de longitude O. <br>La tour du phare de Gravelines a 25 mètres 50 de hauteur au-dessus du sol, et le foyer de l&rsquo;appareil d&rsquo;éclairage domine de 20 mètres le niveau des plus hautes marées. <br>Dans un beau temps, le nouveau feu pourra être aperçu jusqu&rsquo;à une distance de 15 milles marins. </p>
+
+
+
+        <h2>1860 Conseil général</h2>
+
+
+
+        <p>Le port de Dunkerque présente en Conseil Général un projet pour améliorer la navigation dans les bancs de Flandres :  remplacer la grosse bouée d&rsquo;avertissement de l&rsquo;ouest, à Dunkerque, par un feu flottant.</p>
+
+
+
+        <h2>1862</h2>
+
+
+
+        <p>Le Ministre de l&rsquo;Agriculture, du Commerce et des Travaux publics autorise l&rsquo;établissement de trois feux flottants dans la rade de Dunkerque.</p>
+
+
+
+        <p>&#8211; Le premier de ces feux sera placé près de l&rsquo;extrémité nord du banc connu sous le nom de Cliff d&rsquo;Islande avec une portée de 11 milles, il servira de point d&rsquo;atterrage aux bâtiments venant de l&rsquo;est et du nord-est.</p>
+
+
+
+        <p>&#8211; Le second feu flottant sera mouillé à près de 3 milles au N.N.-E. du phare de Gravelines ; sa portée sera comme celle du premier de 11 milles. Pris par le phare de Dunkerque, il fera éviter aux navigateurs venant de l&rsquo;ouest tous les bancs voisins de leur route ; pris par le phare de Gravelines, il jalonnera une ligne passant d&rsquo;abord par les deux Ruytingen, puis entre les Dycks, de sorte que les navigateurs ne seront pas obligés de se porter à l&rsquo;ouest pour aller chercher un chenal offrant plus de sécurité. Ce feu aura comme le premier une portée de 11 milles.</p>
+
+
+
+        <p>Le troisième feu flottant sera mouillé à 2,4 milles dans le N.N.-0. de Mardyck et aura une portée de 6 à 7 milles ; son alignement avec le précédent donnera le gisement de la rade de Dunkerque.</p>
+
+
+
+        <p>La même décision ministérielle a prescrit l&rsquo;établissement d&rsquo;un feu fixe vert de 3 à 4 milles de portée pour signaler l&rsquo;extrémité de la jetée orientale du port de Dunkerque.</p>
+
+
+
+        <p>Le 23 juin un projet pour l&rsquo;exécution des navires destinés à porter les feux flottants est approuvé par le Ministre des TravauxPpublics.</p>
+
+
+
+        <h2>1863</h2>
+
+
+
+        <p>Une décision ministérielle du 3 juillet 1862 assigne à ces trois feux flottants les caractéristiques suivantes :</p>
+
+
+
+        <p><strong>Feu flottant du Cliff d&rsquo;Islande</strong>. Feu à éclipses, dans lequel un éclat rouge alternera avec deux éclats blancs.</p>
+
+
+
+        <p><strong>Feu flottant de Gravelines.</strong>&nbsp;Feu à éclipses avec éclats rouges.</p>
+
+
+
+        <p><strong>Feu flottant de Mardyck.</strong>&nbsp;Feu fixe rouge.</p>
+
+
+
+        <p><strong>Les feux flottants dits Ruytingen et Mardyck, destinés à éclairer la rade de Dunkerque, ont été allumés le 15 novembre 1863.</strong></p>
+
+
+
+        <h2>1864</h2>
+
+
+
+        <p>Le troisième, un feu à éclipses, dans lequel un éclat rouge devait alterner avec deux éclats blancs, devait être placé près de l&rsquo;extrémité nord du banc connu sous le nom de Cliff d&rsquo;Islande, avoir une portée de 11 milles, et servir de point d&rsquo;atterrage aux bâtiments venant de la partie de l&rsquo;Est et du Nord-Est.</p>
+
+
+
+        <p>Mais un avis aux navigateurs, émané du gouvernement belge, ayant annoncé qu&rsquo;un feu flottant à éclats, alternativement blancs et rouges, devait être installé dans le courant du mois de mars 1864, sur l&rsquo;extrémité S.-O. du ban de West-Hinder (ce feu a été allumé le 4 avril dernier) , ce feu étant appelé à remplir le même office et présentant à peu près le même caractère que celui qui avait été adopté en principe pour être mouillé à l&rsquo;extrémité nord du Cliff d&rsquo;Islande celui-ci ne sera pas mis en place.</p>
+
+
+
+        <p>L&rsquo;allumage des feux flottants de la rade de Dunkerque a réalisé une amélioration très importante dont se louent hautement tous les navigateurs.</p>
+
+
+
+        <h2>1866</h2>
+
+
+
+        <p>En 1865, M. le Ministre de l&rsquo;Agriculture, du Commerce et des Travaux publics avait prescrit de faire de nouvelles études générales sur l&rsquo;ensemble du balisage des bancs de la Flandre française ainsi que sur les positions à assigner aux bouées destinées à indiquer la limite septentrionale de ces bancs.</p>
+
+
+
+        <p>A la suite de ces études est intervenue, le 12 mai dernier, une décision par laquelle M. le Ministre a fixé les positions définitives que devront occuper les bouées devant continuer le balisage extérieur des bancs de Flandre, a prescrit de reporter les feux flottants actuels, <strong>Mardyck et Ruytingen</strong>, le premier de 1 mille, le second de 2 milles 1/2 vers l&rsquo;ouest et de donner à ce dernier deux mâts supportant chacun un feu fixe blanc, enfin, a autorisé l&rsquo;établissement d&rsquo;un nouveau feu flottant, lequel, mouillé à l&rsquo;extrémité nord-est du banc de l&rsquo;Out-Ruytingen, sera surmonté d&rsquo;un appareil à éclats rouges se succédant de 30 secondes en 30 secondes, et ayant une portée de 11 milles environ.</p>
+
+
+
+        <h2>1869</h2>
+
+
+
+        <p>L&rsquo;éclairage des ports, rades et passes navigables du Nord comporte un phare et des feux de marée à Gravelines ; un phare, un feu sur le&nbsp;<strong>Leugenaer</strong>, et des feux de marée à Dunkerque ; enfin, trois feux flottants dont deux, le «&nbsp;<strong>Dyck</strong>&nbsp;» et le «&nbsp;<strong>Snow</strong>&nbsp;», jalonnent l&rsquo;entrée ouest de la rade de Dunkerque, tandis que le troisième, le «&nbsp;<strong>Ruytingen</strong>, » signale en même temps l&rsquo;extrémité du banc de l’Out-Ruytingen et la route à suivre pour éviter les bancs de Flandre en allant à Calais.</p>
+
+
+
+        <h2>1884</h2>
+
+
+
+        <p>Pour répondre à un vœu formulé par une commission nautique et tendant à voir augmenter la portée des feux qui jalonnent l&rsquo;entrée de la rade, nous présenterons en juillet 1884 à l&rsquo;examen de l&rsquo;administration supérieure des propositions ayant pour but d&rsquo;essayer la substitution de l&rsquo;éclairage par le gaz riche à l&rsquo;éclairage par l&rsquo;huile de schiste pour le  <strong>Dyck</strong> . Si l&rsquo;administration accueille nos propositions et si l&rsquo;essai réussit, la modification apportée dans l&rsquo;éclairage du   &nbsp;<strong>Dyck</strong>  réalisera le vœu des marins qui fréquentent le port de Dunkerque, d&rsquo;abord en augmentant la portée lumineuse du principal feu d&rsquo;entrée de la rade, puis en rendant sa portée géographique constante par l&rsquo;adoption d&rsquo;un appareil léger, capable d&rsquo;être maintenu à la tête du mât en tout temps. Aujourd&rsquo;hui on est obligé de descendre les feux souvent à minima, à cause du poids des appareils qui ne pourraient être maintenus, par gros temps, à leur hauteur réglementaire.</p>
+
+
+
+        <p><strong>Le phare de Dunkerque est éclairé à l&rsquo;électricité depuis le 1er octobre 1885 : le feu nouveau est scintillant, à groupes de deux éclats blancs.</strong></p>
+
+
+
+        <h2>1887</h2>
+
+
+
+        <p>Phares. L&rsquo;éclairage du phare à l&rsquo;électricité est satisfaisant.</p>
+
+
+
+        <p>Feux flottants. — On s&rsquo;occupe de l&rsquo;amélioration de ces feux, souvent, recommandée par le Conseil Général. Celui du «<strong>Dyck</strong>» va être rapproché de Dunkerque, Ses deux feux vont être remplacés par un feu unique, tournant, à éclipses, beaucoup plus puissant, comportant tous les perfectionnements apportés aux feux anglais.</p>
+
+
+
+        <h2>1888</h2>
+
+
+
+        <p>Les trois feux flottants du «&nbsp;<strong>Snow</strong>&nbsp;», du «&nbsp;<strong>Dyck</strong>&nbsp;» et du «&nbsp;<strong>Ruytingen</strong>&nbsp;» éclairent les abords de Dunkerque.</p>
+
+
+
+        <p>Les marins se plaignent depuis longtemps de l&rsquo;insuffisance de portée et d&rsquo;éclat de ces feux.</p>
+
+
+
+        <p>L&rsquo;administration, par une décision du 17 mars 1886, a reconnu la nécessité d&rsquo;améliorer l&rsquo;éclairage du «<strong>dyck</strong>» et a prescrit de rapprocher le bateau de Dunkerque et de remplacer ses deux feux par un feu unique tournant à éclipses, beaucoup plus puissant, comportant tous les perfectionnements admis dans ces dernières années sur les feux anglais.</p>
+
+
+
+        <p>Les appareils ont été essayés et paraissent devoir donner des résultats satisfaisants. Ils ont été installés sur un bateau en bois, acquis par l&rsquo;État en 1882, qui a été aménagé pour servir de bateau-feu. Le bateau a été mouillé sur l&#8217;emplacement nouveau assigné, et le feu a été allumé le 8 juillet ; on devra atténuer les effets du roulis qui dans le gros temps rendent le service pénible. Un crédit de 37,680 fr a été affecté ace travail sur les exercices de 1887 et 1888.</p>
+
+
+
+        <p>Un crédit de 51,000 fr est ouvert sur l&rsquo;exercice de 1888 pour la restauration du bateau qui sert de rechange aux trois bateaux-feux.</p>
+
+
+
+        <p>Enfin, un crédit de 130,000 fr doit être employé cette année à la construction d&rsquo;un nouveau bateau-feu, en acier, dont le projet est actuellement soumis à l&rsquo;examen d&rsquo;une Commission locale.</p>
+
+
+
+        <h2>1892</h2>
+
+
+
+        <p>L&rsquo;Administration, par une décision du 17 mars 1886, a prescrit de rapprocher le <strong>Dyck</strong> de Dunkerque et de remplacer ses deux feux par un feu unique tournant à éclipses beaucoup plus puissant, comportant tous les perfectionnements admis dans ces dernières années sur les feux anglais.</p>
+
+
+
+        <p>Le nouveau feu avait été installé en 1888 sur un bateau qui éprouvait dans les gros temps des mouvements de roulis très gênants ; ce bateau, après avoir été muni de quilles de roulis qui ont atténué dans une forte proportion les inconvénients constatés, a été remis en place le 10 juin 1890 et fait depuis cette époque un service très régulier.</p>
+
+
+
+        <p>Un bateau feu en acier, construit en 1889-1890, remplace, depuis le 28 janvier 1891, l&rsquo;ancien «&nbsp;<strong>Ruytingen</strong>&nbsp;». Ce bateau est muni d&rsquo;appareils lumineux semblables à ceux qui sont installés sur le nouveau «&nbsp;<strong>Dyck</strong>&nbsp;» et en outre d&rsquo;une trompette mue par des machines à air chaud.</p>
+
+
+
+        <h2>1902</h2>
+
+
+
+        <p>Mise à l’eau du grand bateau feu <strong>Sandettie</strong> aux chantiers Dyle et Bacalan de Bordeaux.</p>
+
+
+
+        <p></p>
+
+
+
+        <p class="has-text-align-left has-background" style="background-color:#c8dbd4"><strong>Fortunes de mer et naufrages des bateaux feux</strong><br><sup><br>21 novembre 1893 <a href="https://sauveteurdudunkerquois.fr/93ruy-0385b/">le Ruytingen casse ses chaines</a><br>2 et 3 février 1902  <a href="https://sauveteurdudunkerquois.fr/02ruy-0423b/">Abordage du  Ruytingen </a> et le <a href="https://sauveteurdudunkerquois.fr/02dyc-0897b/">Dyck casse ses chaines</a><br>10 septembre 1903 <a href="https://sauveteurdudunkerquois.fr/03san-0543b/">Abordage du Sandettie</a><br>30 juin 1906 Abordage ou incendie sur le  <a href="https://sauveteurdudunkerquois.fr/06ruy-0421b/">Ruytingen</a> <br>25 mai 1911 <a href="https://sauveteurdudunkerquois.fr/estaminet02a/">Un bateau-feu flottant a disparu  (Article de presse)</a><br>13 décembre 1933 <a href="https://sauveteurdudunkerquois.fr/33dyc-0531b/">Naufrage du Dyck</a><br>25 mai 1940 <a href="https://sauveteurdudunkerquois.fr/40dyc-0732b/">Le Dyck victime d&rsquo;une attaque de la Luftwaffe</a></sup></p>
+
+
+
+        <p></p>
+
+
+
+        <p><span class="has-inline-color has-black-color"><sup>Sources</sup><br><sup>Anna</sup></span><sup><span class="has-inline-color has-black-color">les de la Société Centrale de Sauvetage des Naufragés<br>Archives Municipales de Dunkerque (CUD)<br>Dessin de Jean-François Parent Musée portuaire de Dunkerque<br>La Dunkerquoise 23 février 1843</span></sup></p>
+
+
+    </div>
+
+</article>
